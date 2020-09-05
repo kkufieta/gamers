4ef9e4f95878ca495accb6a403ffc6db0349f0f5
kkufieta <katharina.kufieta@gmail.com>
2020-09-05 11:32:10 -0400

    Add event handlers to form inputs

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 
 class InputForm extends React.Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        username: ''
+    }
+
+    handleFirstNameChange = event => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleLastNameChange = event => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleUsernameChange = event => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
     render() {
         return (
             <div className="uk-container">
@@ -12,6 +36,8 @@ class InputForm extends React.Component {
                                 type="text"
                                 className="uk-input"
                                 placeholder="First Name"
+                                value={this.state.firstName}
+                                onChange={this.handleFirstNameChange}
                             />
                         </div>
                         <div className="uk-margin">
@@ -19,6 +45,8 @@ class InputForm extends React.Component {
                                 type="text"
                                 className="uk-input"
                                 placeholder="Last Name"
+                                value={this.state.lastName}
+                                onChange={this.handleLastNameChange}
                             />
                         </div>
                         <div className="uk-margin">
@@ -26,13 +54,15 @@ class InputForm extends React.Component {
                                 type="text"
                                 className="uk-input"
                                 placeholder="Username"
+                                value={this.state.username}
+                                onChange={this.handleUsernameChange}
                             />
                         </div>
                     </fieldset>
                 </form >
                 <button className="uk-align-center uk-button uk-button-default">
                     Submit
-                    </button>
+                </button>
             </div >
 
         )
